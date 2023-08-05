9bd41818e68bb432fe1e13e38b65e579c7160d2f
AnnSha <59201702+AnnSha@users.noreply.github.com>
2023-08-05 14:28:50 +0200

    Update AnecdoteForm.js

--- anecdotes/components/AnecdoteForm.js
+++ anecdotes/components/AnecdoteForm.js
@@ -1,1 +1,25 @@
+import { useDispatch } from 'react-redux'
+import { createAnecdote } from '../reducers/anecdoteReducer'
+import { createNotification} from "../reducers/notificationReducer";
 
+const AnecdoteForm = () => {
+    const dispatch = useDispatch()
+
+    const addAnecdote = async (event) => {
+        event.preventDefault()
+        const content = event.target.anecdote.value
+        event.target.anecdote.value = ''
+      dispatch(createAnecdote(content))
+        dispatch(createNotification(`Add: ${content}`))
+    }
+
+
+    return (
+        <form onSubmit={addAnecdote}>
+            <input name="anecdote" />
+            <button type="submit">add</button>
+        </form>
+    )
+}
+
+export default AnecdoteForm
