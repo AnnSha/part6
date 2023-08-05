a74be545fbdcb7eb5f27542a903a834a9c6873e2
AnnSha <59201702+AnnSha@users.noreply.github.com>
2023-08-05 14:35:12 +0200

    Update notificationReducer.js

--- anecdotes/reducers/notificationReducer.js
+++ anecdotes/reducers/notificationReducer.js
@@ -1,1 +1,20 @@
 
+import { createSlice } from '@reduxjs/toolkit'
+
+const notificationSlice = createSlice({
+    name: 'notification',
+    initialState: 'welcome',
+    reducers: {
+        createNotification(state, action) {
+            return action.payload
+        },
+
+
+        clearNotification() {
+            return null;
+        },
+    },
+})
+
+export const {  createNotification, clearNotification} = notificationSlice.actions
+export default notificationSlice.reducer
