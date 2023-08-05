0412a72feb586a411259e14b1bc49af1b5bfdfc7
AnnSha <59201702+AnnSha@users.noreply.github.com>
2023-08-05 14:30:35 +0200

    Update Filter.js

--- anecdotes/components/Filter.js
+++ anecdotes/components/Filter.js
@@ -1,1 +1,27 @@
+import { filterChange } from '../reducers/filterReducer'
+import {useDispatch} from "react-redux";
 
+const Filter = () => {
+    const dispatch = useDispatch()
+    const handleChange = (event) => {
+        // const filterValue = event.target.value;
+        dispatch(filterChange(event.target.value));
+    };
+    const style = {
+        marginBottom: 10
+    }
+
+    return (
+        <div style={style}>
+            <p>filter :</p>
+            <input type='search'
+                   placeholder='search...'
+                   name="filter"
+                   onChange={handleChange}
+            />
+
+        </div>
+    )
+}
+
+export default Filter
