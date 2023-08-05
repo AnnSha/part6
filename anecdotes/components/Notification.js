5e67526c54543fdbefd4a92f78b0255ae34f02d4
AnnSha <59201702+AnnSha@users.noreply.github.com>
2023-08-05 14:31:48 +0200

    Update Notification.js

--- anecdotes/components/Notification.js
+++ anecdotes/components/Notification.js
@@ -1,1 +1,38 @@
+import {useDispatch, useSelector} from 'react-redux'
+import {useEffect} from "react";
+import {clearNotification} from "../reducers/notificationReducer";
 
+
+const Notification = () => {
+  const dispatch = useDispatch()
+  const notification = useSelector((state) => state.notification);
+
+  useEffect(() => {
+
+    if (notification) {
+      const timer = setTimeout(() => {
+        dispatch(clearNotification());
+      }, 5000);
+
+      return () => {
+        clearTimeout(timer);
+      }
+    }
+  }, [dispatch, notification]);
+
+  const style = {
+    border: 'solid',
+    padding: 10,
+    borderWidth: 1
+  }
+  if (!notification) {
+    return null
+  }
+  return (
+      <div style={style}>
+        {notification}
+      </div>
+  )
+}
+
+export default Notification
